acb000682afc8e68345305e06cb60160994e8020
Roberto Acosta <x.rojo@hotmail.com>
2020-09-29 05:39:27 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,8 +18,8 @@ end
 
 def literal_aoh
   literal_aoh = [
-    {:name=>  "Don Gately", :occupation=> "Live-in Staff" }
-    {:name=>  "Joelle van Dyne", :occupation=> "Radio Personality" }
+    {:name =>  "Don Gately", :occupation => "Live-in Staff" }
+    {:name =>  "Joelle van Dyne", :occupation => "Radio Personality" }
     ]
 end
 
